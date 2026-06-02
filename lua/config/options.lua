82a518733e0e61f342695db30597125fe3155d29
Derek Riemer <git@derekriemer.com>
2026-06-02 20:44:17 +0000

    clipboard fixes

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -10,3 +10,29 @@ vim.opt.shellcmdflag =
 
 vim.opt.shellquote = ""
 vim.opt.shellxquote = ""
+
+local osc52_hosts = {
+  ["derekriemer.com"] = true,
+  ["el-zorro"] = true,
+}
+
+local hostname = vim.uv.os_gethostname()
+local is_ssh_session = vim.env.SSH_CONNECTION ~= nil
+  or vim.env.SSH_CLIENT ~= nil
+  or vim.env.SSH_TTY ~= nil
+
+if osc52_hosts[hostname] or is_ssh_session then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+
+  vim.opt.clipboard = "unnamedplus"
+end
