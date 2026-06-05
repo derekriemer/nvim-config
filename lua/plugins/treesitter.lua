077d019e012f109f778b6df8a0edfd6731c725a7
Derek Riemer <git@derekriemer.com>
2026-06-05 18:11:44 +0000

    chore(nvim): migrate treesitter to main

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,27 +1,33 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    branch = "master",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
 
     config = function()
-      require("nvim-treesitter.configs").setup({
-        highlight = {
-          enable = true,
-        },
+      local treesitter = require("nvim-treesitter")
+      local parsers = {
+        "lua",
+        "rust",
+        "python",
+        "javascript",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "markdown",
+        "markdown_inline",
+      }
 
-        incremental_selection = {
-          enable = true,
+      treesitter.setup()
 
-          keymaps = {
-            init_selection = "<leader>ss",
-            node_incremental = "<leader>si",
-            node_decremental = "<leader>sd",
-            scope_incremental = "<leader>sc",
-          },
-        },
+      if vim.fn.executable("tree-sitter") == 1 then
+        treesitter.install(parsers)
+      end
 
-        ensure_installed = {
+      vim.api.nvim_create_autocmd("FileType", {
+        group = vim.api.nvim_create_augroup("derek_treesitter", { clear = true }),
+        pattern = {
           "lua",
           "rust",
           "python",
@@ -29,13 +35,22 @@ return {
           "typescript",
           "vim",
           "vimdoc",
+          "markdown",
         },
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
       })
+
+      vim.keymap.set("n", "<leader>ss", "van", { desc = "Start tree-sitter selection", remap = true })
+      vim.keymap.set("x", "<leader>si", "an", { desc = "Increment tree-sitter selection", remap = true })
+      vim.keymap.set("x", "<leader>sd", "in", { desc = "Decrement tree-sitter selection", remap = true })
     end,
   },
 
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
     },
