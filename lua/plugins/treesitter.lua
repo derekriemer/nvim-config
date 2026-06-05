64f658d6408c4c8bd8a763e8a0fed6db1cbd47fb
Derek Riemer <git@derekriemer.com>
2026-06-05 19:29:58 +0000

    feat(nvim): add treesitter markdown navigation

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -54,5 +54,46 @@ return {
     dependencies = {
       "nvim-treesitter/nvim-treesitter",
     },
+
+    config = function()
+      require("nvim-treesitter-textobjects").setup({
+        move = {
+          set_jumps = true,
+        },
+      })
+
+      local move = require("nvim-treesitter-textobjects.move")
+
+      local function next_start(capture, group)
+        return function()
+          move.goto_next_start(capture, group)
+        end
+      end
+
+      local function previous_start(capture, group)
+        return function()
+          move.goto_previous_start(capture, group)
+        end
+      end
+
+      local maps = {
+        { "]h", next_start("@heading.outer", "textobjects"), "Next heading" },
+        { "[h", previous_start("@heading.outer", "textobjects"), "Previous heading" },
+        { "]s", next_start("@section.outer", "textobjects"), "Next section" },
+        { "[s", previous_start("@section.outer", "textobjects"), "Previous section" },
+        { "]l", next_start("@list.outer", "textobjects"), "Next list" },
+        { "[l", previous_start("@list.outer", "textobjects"), "Previous list" },
+        { "]L", next_start("@list_item.outer", "textobjects"), "Next list item" },
+        { "[L", previous_start("@list_item.outer", "textobjects"), "Previous list item" },
+        { "]b", next_start("@block.outer", "textobjects"), "Next block" },
+        { "[b", previous_start("@block.outer", "textobjects"), "Previous block" },
+        { "]f", next_start("@fold", "folds"), "Next fold" },
+        { "[f", previous_start("@fold", "folds"), "Previous fold" },
+      }
+
+      for _, map in ipairs(maps) do
+        vim.keymap.set({ "n", "x", "o" }, map[1], map[2], { desc = map[3] })
+      end
+    end,
   },
 }
---
+++ queries/markdown/textobjects.scm
@@ -0,0 +1,11 @@
+; extends
+
+(atx_heading) @heading.outer
+
+(setext_heading) @heading.outer
+
+(section) @section.outer
+
+(list) @list.outer
+
+(list_item) @list_item.outer
